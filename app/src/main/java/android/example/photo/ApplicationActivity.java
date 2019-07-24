95f5b7a9f8f7f00c0d5305dd8458e911c1edc08e
Valeriya <valeriya.ridchenko@mail.ru>
2019-07-24 12:23:46 +0300

    display photo

--- app/src/main/java/android/example/photo/ApplicationActivity.java
+++ app/src/main/java/android/example/photo/ApplicationActivity.java
@@ -1,5 +1,10 @@
 package android.example.photo;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.example.photo.Fragments.FavoritesFragment;
 import android.example.photo.Fragments.InfoFragment;
 import android.example.photo.Fragments.PhotosFragment;
@@ -11,9 +16,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -22,9 +31,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApplicationActivity extends AppCompatActivity {
 
+    DBHelper dbHelper;
+    SQLiteDatabase db;
+
     private boolean loadFragment(Fragment fragment){
         if(fragment != null){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
             return true;
         }
         return false;
@@ -59,7 +72,11 @@ public class ApplicationActivity extends AppCompatActivity {
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navView.setSelectedItemId(R.id.navigation_photos);
         loadFragment(new PhotosFragment());
-
+        final String[] url = {null};
+        dbHelper = new DBHelper(this);
+        getRandomPhotoUrl();
+    }
+    private String getRandomPhotoUrl(){
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.unsplash.com/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -75,7 +92,8 @@ public class ApplicationActivity extends AppCompatActivity {
                     System.out.println("Code: " + response.code());
                     return;
                 }
-                System.out.println("!!! I am onResponse" + response.body().getUrls().getFull());
+                //System.out.println(response.body().getUrls().getFull());
+                setUrlOnDB(response.body().getUrls().getFull());
             }
 
             @Override
@@ -84,6 +102,53 @@ public class ApplicationActivity extends AppCompatActivity {
                 System.out.println(t.getMessage());
             }
         });
+        System.out.println("!!! I am onResponse" + call.request().body());
+        return "";
     }
+    private void setUrlOnDB(String url){
+        System.out.println("_______________________________________" + url);
+        ContentValues cv = new ContentValues();
+        db = dbHelper.getWritableDatabase();
+        cv.put("url", url);
+        //db.insert("urlsTable", null, cv);
+    }
+    private String getUrlFromDB(Integer id){
+        String url = null;
+        Cursor c = db.query("urlsTable", null,null, null,
+                null, null, null);
+        Integer i = 0;
+        if(c.moveToFirst()) {
+            do{
+                if(id == i){
+                    url = c.getString(c.getColumnIndex("url"));
+                    return url;
+                }
+            }while(c.moveToNext());
+        }
+        else
+            System.out.println("EMPTY");
+        c.close();
+        return "error";
+    }
+    public static class DBHelper extends SQLiteOpenHelper {
+        //вроде был private not static
+        public DBHelper(Context context) {
+            // конструктор суперкласса
+            super(context, "photoDB", null, 1);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            //Log.d(LOG_TAG, "--- onCreate database ---");
+            // создаем таблицу с полями
+            db.execSQL("create table urlsTable ("
+                    + "id integer primary key autoincrement,"
+                    + "url text" + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+}
 
 }
--- app/src/main/java/android/example/photo/Fragments/PhotosFragment.java
+++ app/src/main/java/android/example/photo/Fragments/PhotosFragment.java
@@ -1,16 +1,83 @@
 package android.example.photo.Fragments;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.example.photo.ApplicationActivity;
 import android.example.photo.R;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class PhotosFragment extends Fragment {
+
+    ApplicationActivity.DBHelper dbHelper;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        dbHelper = new ApplicationActivity.DBHelper(getContext());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
         return inflater.inflate(R.layout.photo, null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        View childView;
+        //TODO: добавить цикл на 50 элементов, который берет ссылки из БД
+        //TODO: добавить онкликлистенер и при нажатии на него ссылка (или ид в БД) передается в интент, который создает активити без тулбара
+        //TODO: можно потом добавить номер картинки где-то (или при нажатии шобы появлялся, мона брать ид из бд)
+        LayoutInflater inflaterCurrent = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View main = inflaterCurrent.inflate(R.layout.photo, null);
+        TextView tv = main.findViewById(R.id.tvTest);
+        System.out.println(tv.getText());
+        tv.setText("hello");
+        LinearLayout scrollLayout = view.findViewById(R.id.container_photo);
+        //Cursor c = db.query("testTable", null,null, null,
+        //        null, null, null);
+        childView = inflaterCurrent.inflate(R.layout.inflate_picture_in_list, null);
+        WebView wv = new WebView(getActivity());//childView.findViewById(R.id.wvInList);////
+        //wv.loadUrl("https://images.unsplash.com/photo-1561842318-18fbed8d47eb?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjgyODgzfQ");
+        wv.loadUrl("https://images.unsplash.com/photo-1561957119-d628e4bff3a9?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjgyODgzfQ");
+        //https://images.unsplash.com/photo-1561957119-d628e4bff3a9?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjgyODgzfQ
+
+        //wv.setInitialScale(30);
+        wv.setInitialScale(1);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setLoadWithOverviewMode(true);
+        wv.getSettings().setUseWideViewPort(true);
+        wv.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        wv.setScrollbarFadingEnabled(false);
+        //wv.getSettings().setLoadWithOverviewMode(true);
+        //wv.getSettings().setUseWideViewPort(true);
+        scrollLayout.addView(wv);
+        ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) wv.getLayoutParams();
+        p.bottomMargin = 30;
+        wv.setLayoutParams(p);
+        WebView wv1 = new WebView(getActivity());//childView.findViewById(R.id.wvInList);////
+        wv1.loadUrl("https://images.unsplash.com/photo-1561842318-18fbed8d47eb?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjgyODgzfQ");
+        //wv1.loadUrl("https://images.unsplash.com/photo-1561957119-d628e4bff3a9?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjgyODgzfQ");
+        //https://images.unsplash.com/photo-1561957119-d628e4bff3a9?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjgyODgzfQ
+        wv1.setInitialScale(30);
+        wv1.setInitialScale(1);
+        wv1.getSettings().setJavaScriptEnabled(true);
+        wv1.getSettings().setLoadWithOverviewMode(true);
+        wv1.getSettings().setUseWideViewPort(true);
+        wv1.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        wv1.setScrollbarFadingEnabled(false);
+        //wv.getSettings().setLoadWithOverviewMode(true);
+        //wv.getSettings().setUseWideViewPort(true);
+        scrollLayout.addView(wv1);
+
+
     }
 }
